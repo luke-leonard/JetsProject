5b5dfcc2c52dc8d0ed15e8b1e426c1b5c0190c54
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2019-11-27 16:03:49 -0700

    initial commit

---
+++ src/com/skilldistillery/jets/AirField.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.jets;
+
+public class AirField {
+
+	public AirField() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/skilldistillery/jets/FighterJet.java
@@ -0,0 +1,28 @@
+package com.skilldistillery.jets;
+
+public class FighterJet extends Jet implements CombatReady {
+
+	public FighterJet(String model, double speed, int range, long price) {
+		super(model, speed, range, price);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void fight() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double getSpeedInMach() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
